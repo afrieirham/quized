8df9b423601e72eb197ef08ad4b3e1f62ece4781
afrieirham <itsafrie@gmail.com>
2020-02-09 23:30:13 +0800

    + Add result, quiz, and question model + Add result, quiz, and question controller

---
+++ app/Controllers/Http/ResultController.js
@@ -0,0 +1,86 @@
+'use strict';
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with results
+ */
+class ResultController {
+	/**
+	 * Show a list of all results.
+	 * GET results
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 * @param {View} ctx.view
+	 */
+	async index({ request, response, view }) {}
+
+	/**
+	 * Render a form to be used for creating a new result.
+	 * GET results/create
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 * @param {View} ctx.view
+	 */
+	async create({ request, response, view }) {}
+
+	/**
+	 * Create/save a new result.
+	 * POST results
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 */
+	async store({ request, response }) {}
+
+	/**
+	 * Display a single result.
+	 * GET results/:id
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 * @param {View} ctx.view
+	 */
+	async show({ params, request, response, view }) {}
+
+	/**
+	 * Render a form to update an existing result.
+	 * GET results/:id/edit
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 * @param {View} ctx.view
+	 */
+	async edit({ params, request, response, view }) {}
+
+	/**
+	 * Update result details.
+	 * PUT or PATCH results/:id
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 */
+	async update({ params, request, response }) {}
+
+	/**
+	 * Delete a result with id.
+	 * DELETE results/:id
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 */
+	async destroy({ params, request, response }) {}
+}
+
+module.exports = ResultController;
